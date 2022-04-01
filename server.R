e2545cca209232030b080ef015a2a6cb861987c5
phillip <pc494@cam.ac.uk>
2022-04-01 14:56:03 +0100

    EOD April 1st

--- server.R
+++ server.R
@@ -1,5 +1,13 @@
 function(input, output, session){
-  
+  output$var <- renderUI({
+    tags <- tagList()
+    for (i in seq_len(input$K)) {
+      tags[[i]] <- numericInput(paste0('K', i), 
+                                paste0('Sigma Value ', i),
+                                0)
+    }
+    tags
+  })
 }
 
 #observeEvent(input$min, {
--- ui.R
+++ ui.R
@@ -1,26 +1,13 @@
 fluidPage(
-  # fluidRow(
-  #   box(width = 12, title = "A Box in a Fluid Row I want to Split", 
-  #       splitLayout(
-  #         textInput("inputA", "The first input"),
-  #         textInput("inputB", "The second input"),
-  #         textInput("inputC", "The third input")
-  #                 )
-  #       )
-  #   ),
-  numericInput(
-    "K",
-    "Number of Trials",
-    5,
-    min = 1,
-    max = 1000,
-    step = 1,
-    width = NULL
-  ))
-  # sliderInput("Figure_Number",
-  #             label = "Figure Number",
-  #             min = 1,
-  #             max = 5,
-  #             value = 1),
-  # plotOutput("curve_plot")
-
+  fluidRow(
+    column(width=3,
+          numericInput("K",
+                       "Number of Trials",
+                        5,min = 1,max = 1000,step = 1,width = NULL)
+          ),
+    column(width=3,
+           uiOutput('var')
+          )
+          )
+)
+  
