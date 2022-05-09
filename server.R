8b000013b40bad439984e987aa9120ce5bfbf809
phillip <pc494@cam.ac.uk>
2022-05-09 16:58:44 +0100

    Still fighting the subscripts

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ function(input, output, session){
     tags <- tagList()
     for (i in seq_len(input$K)) {
       tags[[i]] <- numericInput(paste0('K', i), 
-                                HTML(paste0('\u03C3',tags$sub(i))),
+                                HTML(paste0('\u03C3_{',i,'}')),
                                 value=0)
     }
     tags
@@ -12,10 +12,11 @@ function(input, output, session){
     tags <- tagList()
     for (i in seq_len(input$K)) {
       tags[[i]] <- numericInput(paste0('K', i), 
-                                paste0('R', i),
+                                paste0('R_{', i,'}'),
                                 value=0)
     }
     tags
   })
   
 }
+#tags$sub(i))
--- ui.R
+++ ui.R
@@ -1,9 +1,10 @@
+col_width <- 2
 fluidPage(
           fluidRow(
-                  column(width=3,
+                  column(width=col_width,
                         numericInput("K","Number of Trials",5,min = 1,max = 20,step = 1,width = NULL)),
-                  column(width=3,
-                        numericInput("ssq","s²",100,min = 1,max = 1000,step = 1,width = NULL),
+                  column(width=col_width,
+                        numericInput("ssq","s_{0k}²",100,min = 1,max = 1000,step = 1,width = NULL),
                         numericInput("a1","a₁",2,min = 1,max = 20,step = 1,width = NULL),
                         numericInput("b1","b₁",2,min = 1,max = 20,step = 1,width = NULL),
                         numericInput("a2","a₂",54,min = 1,max = 1000,step = 1,width = NULL),
@@ -11,8 +12,8 @@ fluidPage(
                         numericInput("eta","\u03B7",0.95,min = 0,max = 5,step = 1,width = NULL),
                         numericInput("zeta","	\u03B6",0.8,min = 0,max = 5,step = 1,width = NULL)
                         ),
-                  column(width=3,uiOutput('variance')),
-                  column(width=3,uiOutput('allocratio'))
+                  column(width=col_width,uiOutput('variance')),
+                  column(width=col_width,uiOutput('allocratio'))
                   )
           )
   
