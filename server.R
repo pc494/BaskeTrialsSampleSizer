8e005f644177860e254b78f2021f3ce6096fae1c
phillip <pc494@cam.ac.uk>
2022-06-17 15:06:55 +0100

    Close of Play June 17th

--- server.R
+++ server.R
@@ -1,22 +1,34 @@
 function(input, output, session){
+  ### Sigma list ####
   output$variance <- renderUI({
     tags <- tagList()
     for (i in seq_len(input$K)) {
-      tags[[i]] <- numericInput(paste0('K', i), 
+      tags[[i]] <- numericInput(paste0('K', i),
                                 HTML(paste0('\u03C3_{',i,'}')),
                                 value=0)
     }
     tags
   })
-  output$allocratio <- renderUI({
+  ### R list ###
+  reactive_use_equal_alloc_ratio<-reactive({input$use_equal_alloc_ratio_bool})
+  output$allocratio <- renderUI(
+    if (reactive_use_equal_alloc_ratio()){
+      # This means we do not render an R column
+    }
+    else
+    {
     tags <- tagList()
     for (i in seq_len(input$K)) {
-      tags[[i]] <- numericInput(paste0('K', i), 
+      tags[[i]] <- numericInput(paste0('K', i),
                                 paste0('R_{', i,'}'),
-                                value=0)
+                                value=0.5)
     }
     tags
   })
-  
+
+  ### Button to display inputs ####
+  output$value <- renderText({
+    input$update
+    isolate(paste0("K = ",input$K,", the value of eta is: ", input$eta, " and " , input$use_equal_alloc_ratio_bool))
+  })
 }
-#tags$sub(i))
--- ui.R
+++ ui.R
@@ -1,4 +1,6 @@
-col_width <- 2
+library(shiny)
+library(shinyWidgets)
+col_width <- 3
 fluidPage(
           fluidRow(
                   column(width=col_width,
@@ -9,11 +11,24 @@ fluidPage(
                         numericInput("b1","b₁",2,min = 1,max = 20,step = 1,width = NULL),
                         numericInput("a2","a₂",54,min = 1,max = 1000,step = 1,width = NULL),
                         numericInput("b2","b₂",3,min = 1,max = 20,step = 1,width = NULL),
-                        numericInput("eta","\u03B7",0.95,min = 0,max = 5,step = 1,width = NULL),
-                        numericInput("zeta","	\u03B6",0.8,min = 0,max = 5,step = 1,width = NULL)
+                        numericInput("eta","\u03B7",0.95,min = 0,max = 5,step = 0.05,width = NULL),
+                        numericInput("zeta","	\u03B6",0.8,min = 0,max = 5,step = 0.05,width = NULL)
                         ),
                   column(width=col_width,uiOutput('variance')),
-                  column(width=col_width,uiOutput('allocratio'))
-                  )
+                  column(width=col_width,uiOutput('allocratio')),
+                  column(width=col_width),
+                  ),
+          #checkboxInput("use_equal_alloc_ratio_bool", "Use Equal Allocation Ratios", TRUE),
+          materialSwitch(inputId = "use_equal_alloc_ratio_bool", label = "Use Equal Allocation Ratios (False/True)", value=TRUE ,status = "default"),
+          sliderTextInput(
+            inputId = "mySliderText",
+            label = "Level of Borrowing",
+            grid = FALSE,
+            force_edges = TRUE,
+            choices = c("None",
+                        "Low", "High")
+          ),
+          actionButton("update" ,"Calculate", icon("refresh"),
+                       class = "btn btn-primary"),
+          textOutput("value")
           )
-  
