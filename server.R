be2da39469bd4daac0951e50ab65ede72dec8474
phillip <pc494@cam.ac.uk>
2022-07-29 11:01:19 +0100

    Refactor to facilitate the addition of the borrowing method

--- server.R
+++ server.R
@@ -43,29 +43,47 @@ function(input, output, session){
                         sapply(1:reactive_K(), function(i) {input[[paste0("R_", i)]]})}
   })
   
-  # Need to calculate the value of n and C in a reactive environment
-  reactive_n = reactive({NoBrwNi(sigma_vect(),r_vect(),input$eta,input$zeta,input$delta,input$ssq)})
-  
   # write out all the hyper-parameter selections
   
   output$desc <- renderText(paste0("Results calculated with \u03B4=",input$delta,", \u03B7=",input$eta,', \u03B6=',input$zeta))
   
-  # and render a table
-  
   reactive_implemented_bool <- reactive({input$LoB == 'No'})
-  output$value <- 
-     renderUI(
-      if (reactive_implemented_bool())
+  
+  # perform calculations 
+  
+      reactive_n <- reactive({
+        if (reactive_implemented_bool())
         {
-          renderTable(data.frame('k'=1:input$K,
-                                 '\u03C3<sub>k</sub>'=sigma_vect(),
-                                 'n<sub>R</sub>' = as.integer(reactive_n()+0.5),
-                                 'n<sub>T</sub>'=as.integer(reactive_n()*r_vect()+0.5),
-                                 'n<sub>C</sub>'=as.integer(reactive_n()*(1-r_vect())+0.5),
-                                 check.names=FALSE),
-                                 sanitize.colnames.function = function(x) x)
+         NoBrwNi(sigma_vect(),r_vect(),input$eta,input$zeta,input$delta,input$ssq)
         }
-      else{renderText('not implemented')}
+      else{
+        break #so it doesn't throw an error
+        reactive_n <- MyBrwfun(MyNi, 
+                               Ri = r_vect(),
+                               sig02 = sigma_vect(),
+                               s02 = input$ssq,
+                               wiq,
+                               cctrpar = 0.1,
+                               dw,
+                               br,
+                               targEff=input$delta,
+                               eta = input$eta,
+                               zeta = input$zeta)
+      }
+      })
+    # Render Output Table 
+  
+    output$value <- renderUI(
+      if (reactive_implemented_bool())
+      { renderTable(data.frame('k'=1:input$K,
+                               '\u03C3<sub>k</sub>'=sigma_vect(),
+                               'n<sub>R</sub>' = as.integer(reactive_n()+0.5),
+                               'n<sub>T</sub>'=as.integer(reactive_n()*r_vect()+0.5),
+                               'n<sub>C</sub>'=as.integer(reactive_n()*(1-r_vect())+0.5),
+                               check.names=FALSE),
+                               sanitize.colnames.function = function(x) x)
+      }
+      else{renderText('Currently in development')}
      )
   
 }
